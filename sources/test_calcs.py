237e35b97d740436c057c8f96c88da8980fb88b0
Gmarques <gm.marques2010@gmail.com>
2019-03-26 12:06:05 +0000

    Update test_calcs.py

--- sources/test_calcs.py
+++ sources/test_calcs.py
@@ -1,1 +1,28 @@
+import unittest
+import calc
 
+class TestCalc(unittest.TestCase):
+    """
+    Test the add function from the calc library
+    """
+
+
+
+    def test_add_string_and_integer(self):
+        """
+        Test the addition of a string and an integer returns them as one
+        concatenated string (in which the integer is converted to a string)
+        """
+        result = calc.add2('abc', 3)
+        self.assertEqual(result, 'abc3')
+
+    def test_add_string_and_number(self):
+        """
+        Test the addition of a string and a float returns them as one
+        concatenated string (in which the float is converted to a string)
+        """
+        result = calc.add2('abc', '5.5')
+        self.assertEqual(result, 'abc5.5')
+
+if __name__ == '__main__':
+    unittest.main()
