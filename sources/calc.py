bcd4d3e9231e6ea5d38f9a576364842800c01282
Gmarques <gm.marques2010@gmail.com>
2019-03-27 14:15:29 +0000

    Update calc.py

--- sources/calc.py
+++ sources/calc.py
@@ -2,7 +2,7 @@
 The 'calc' library contains the 'add2' function that takes 2 values and adds
 them together. If either value is a string (or both of them are) 'add2' ensures
 they are both strings, thereby resulting in a concatenated result.
-NOTE: If a value submitted to the 'add2' function is a float, it must be done so
+NOTE: If a value submitted tos the 'add2' function is a float, it must be done so
 in quotes (i.e. as a string).
 '''
 
